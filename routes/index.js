cd6be48d10e991a2d75675d150a76cead9000d7c
Zandurk <zandurk@gmail.com>
2020-03-28 14:47:32 +0100

    Upload to generate data for new app

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+
+router.get('/program', function (req, res, next) {
+    res.send(
+        {
+            currentProgram: '16:8',
+            eatStartTime: 12,
+            eatEndTime: 20,
+            totalEatHours: 8,
+            totalFastHours: 16
+        }
+    );
 });
 
 module.exports = router;
